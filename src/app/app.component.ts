a85033466d209f7b7c53d0f94247ac0f4b0ada6b
SebiB24 <sebycool242@gmail.com>
2025-05-17 18:01:07 +0300

    added modals for add, edit and delete + button functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'app-root',
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -25,6 +26,25 @@ export class AppComponent implements OnInit{
       this.employees = response;
       console.log(this.employees);
       });
+  }
+
+  public onOpenModel(employee: any, mode: string): void{
+    const container = document.getElementById('main-container')
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
 }
 
